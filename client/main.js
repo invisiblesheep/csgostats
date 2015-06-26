0036b41f9ced2e1772a6edd0aceec78571deb738
Tuomas Jokioja <tisjok@utu.fi>
2015-06-26 10:41:40 +0300

    mucho grande stuff

--- client/main.js
+++ client/main.js
@@ -1,3 +1,73 @@
+
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+
+
+Template.MainNavigation.helpers({
+
+});
+
+
+
+Template.StatisticsView.helpers({
+  games: function(){
+    return Games.find({userid: Meteor.userId()}, {sort: {createdAt: -1}});
+  },
+
+
+});
+
+Template.StatisticsView.events({
+  "click #submitButton": function(){
+    Games.insert({
+      map: map.value,
+      score: score.value,
+      kills: parseFloat(kills.value),
+      assists: parseFloat(assists.value),
+      deaths: parseFloat(deaths.value),
+      createdAt: new Date(),
+      userid: Meteor.userId()
+    });
+
+    map.value = "";
+    score.value = "";
+    kills.value = "";
+    assists.value = "";
+    deaths.value = "";
+
+    return false;
+  },
+  "click .close": function(){
+    Games.remove(this._id);
+  }
+});
+
+  Template.stats.helpers({
+    kd: function(){
+      var kills = 0;
+      var deaths = 0;
+      var kdratio = 0;
+      Games.find({userid: Meteor.userId()}, {fields: {kills: 1, deaths: 1}}).map(function(doc) {
+        kills += doc.kills;
+        deaths += doc.deaths;
+      });
+      kdratio = kills/deaths;
+      return kdratio.toString();
+    },
+    winp: function(){
+      var winp = 0;
+      var temp;
+      var won = 0;
+      var amount = 0;
+      Games.find({userid: Meteor.userId()}, {fields: {score: 1}}).map(function(doc) {
+        amount = amount + 1;
+        temp = doc.score.split("-");
+        if(temp[0] == "16"){
+          won = won + 1;
+        }
+      });
+      winp = (won/amount)*100;
+      return winp;
+    }
+  });
--- client/routes.js
+++ client/routes.js
@@ -1,40 +1,33 @@
+Router.onBeforeAction(function(){
+  if(! Meteor.userId()){
+    this.layout("GlobalView");
+  } else {
+    this.next();
+  }
+});
+
+Router.onAfterAction(function(){
+  if(! Meteor.userId()){
+    this.layout("GlobalView");
+  } else {
+    this.next();
+  }
+});
+
 Router.route('', function() {
 
     this.layout("GlobalView");
-    /*this.wait(Meteor.subscribe('subjects'), Session.get("currentApplication"));
-    this.wait(Meteor.subscribe('stream-prototypes'), Session.get("currentApplication"));
-    this.wait(Meteor.subscribe('stream-instances'), Session.get("currentApplication"));
-
-    this.wait(this.subscribe("application-streamdata"), Session.get("currentApplication"));*/
-
     if (this.ready()) {
       this.render("HomeView");
     }
-
-    //layoutTemplate: 'GlobalView',
-    //template: 'HomeView',
-    //waitOn: function() {
-    //  return Meteor.subscribe('applications');
-    //}
 });
 
 
 Router.route('/stats', function() {
 
     this.layout("GlobalView");
-    /*this.wait(Meteor.subscribe('subjects'), Session.get("currentApplication"));
-    this.wait(Meteor.subscribe('stream-prototypes'), Session.get("currentApplication"));
-    this.wait(Meteor.subscribe('stream-instances'), Session.get("currentApplication"));
-
-    this.wait(this.subscribe("application-streamdata"), Session.get("currentApplication"));*/
-
     if (this.ready()) {
       this.render("StatisticsView");
     }
 
-    //layoutTemplate: 'GlobalView',
-    //template: 'HomeView',
-    //waitOn: function() {
-    //  return Meteor.subscribe('applications');
-    //}
 });
